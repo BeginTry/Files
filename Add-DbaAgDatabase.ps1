6fc819690af70ecdcf1d9d87b8924f389a9eb6e7
Dave Mason <DMasonSQL@GMail.com>
2018-12-26 17:04:44 -0500

    Update Add-DbaAgDatabase.ps1

--- Add-DbaAgDatabase.ps1
+++ Add-DbaAgDatabase.ps1
@@ -1,17 +1,17 @@
-﻿$User = "sa"
+$User = "sa"
 $PWord = ConvertTo-SecureString -String "Pass@word1" -AsPlainText -Force
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
 
 $SqlInstance = "$env:COMPUTERNAME\SQL2017"
 $AvailabilityGroup = "MyExpenseAG"
-$Query = "SELECT d.name FROM sys.databases d WHERE d.source_database_id IS NULL AND d.recovery_model_desc != 'SIMPLE' AND d.replica_id IS NULL AND d.state_desc = 'ONLINE' AND d.database_id > 4"
+$Query = "SELECT d.name FROM sys.databases d WHERE d.source_database_id IS NULL AND d.recovery_model_desc = 'FULL' AND d.replica_id IS NULL AND d.state_desc = 'ONLINE' AND d.database_id > 4"
 
 $DBList = (Invoke-DbaQuery -SqlInstance $SqlInstance -Database master -Query $Query).name
 
 <#
     Prequisites:
         DB is not a snapshot.
-        DB is in FULL (or BULK_LOGGED?) recovery mode.
+        DB is in FULL recovery mode.
         DB is not already in an Availability Group.
         DB is online.
         DB is not a system database.
@@ -19,14 +19,19 @@ $DBList = (Invoke-DbaQuery -SqlInstance $SqlInstance -Database master -Query $Qu
 #>
 foreach($Database in $DBList)
 {
-    $LastBackup = (Get-DbaLastBackup -SqlInstance $SqlInstance -Database $Database).LastFullBackup
+    $LastBackup = (Get-DbaLastBackup -SqlInstance $SqlInstance -Database $Database)
 
-    if ($LastBackup -eq $null)
+    if ($LastBackup.LastFullBackup -eq $null -or [string]::IsNullOrEmpty($LastBackup.LastFullBackup))
     {
         # Full database backup to default backup directory.
-        Backup-DbaDatabase -SqlInstance $SqlInstance -Database $Database -CompressBackup 
+        Backup-DbaDatabase -SqlInstance $SqlInstance -Database $Database -CompressBackup -Type Full
+    }
+
+    if ($LastBackup.LastLogBackup -eq $null -or [string]::IsNullOrEmpty($LastBackup.LastLogBackup))
+    {
+        # Full database backup to default backup directory.
+        Backup-DbaDatabase -SqlInstance $SqlInstance -Database $Database -CompressBackup -Type Log
     }
 
     Add-DbaAgDatabase -SqlInstance $SqlInstance -AvailabilityGroup $AvailabilityGroup -Database $Database -SqlCredential $Credential -SeedingMode Automatic
 }
-
