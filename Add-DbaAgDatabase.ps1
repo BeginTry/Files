b51a60a81d13e674a58dd882ca0ca12135206209
Dave Mason <DMasonSQL@GMail.com>
2018-12-27 14:13:23 -0500

    Custom Query
    Use Invoke-DbaQuery with a custom query in lieu of Get-DbaLastBackup.
    This reveals which databases are eligible to be added to an Availability Group, and whether or not a backup is needed to establish the log chain for automatic seeding.

--- Add-DbaAgDatabase.ps1
+++ Add-DbaAgDatabase.ps1
@@ -1,37 +1,62 @@
-$User = "sa"
-$PWord = ConvertTo-SecureString -String "Pass@word1" -AsPlainText -Force
-$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
-
 $SqlInstance = "$env:COMPUTERNAME\SQL2017"
 $AvailabilityGroup = "MyExpenseAG"
-$Query = "SELECT d.name FROM sys.databases d WHERE d.source_database_id IS NULL AND d.recovery_model_desc = 'FULL' AND d.replica_id IS NULL AND d.state_desc = 'ONLINE' AND d.database_id > 4"
-
-$DBList = (Invoke-DbaQuery -SqlInstance $SqlInstance -Database master -Query $Query).name
 
 <#
-    Prequisites:
-        DB is not a snapshot.
-        DB is in FULL recovery mode.
-        DB is not already in an Availability Group.
-        DB is online.
-        DB is not a system database.
-        DB has been backed up at least once.
+    Reference: https://docs.microsoft.com/en-us/sql/database-engine/availability-groups/windows/prereqs-restrictions-recommendations-always-on-availability?view=sql-server-2017#PrerequisitesForDbs
+
+    Prerequisites - to be eligible to be added to an availability group, a database must:
+        Be a user database. System databases cannot belong to an availability group.
+        Be a read-write database. Read-only databases cannot be added to an availability group.
+        Be a multi-user database.
+        Not use AUTO_CLOSE.
+        Use the full recovery model (also known as, full recovery mode).
+        Possess at least one full database backup.
+            Note: After setting a database to full recovery mode, a full backup is required to initiate the full-recovery log chain.
+        Not belong to any existing availability group.
+        Not be configured for database mirroring.
+
+        (Additional prerequisites)
+        Not a snapshot.
+        Be online.
 #>
+$Query = "SELECT d.name, s.last_log_backup_lsn
+    FROM master.sys.databases d
+    JOIN master.sys.database_recovery_status s
+	    ON s.database_id = d.database_id
+    LEFT JOIN master.sys.database_mirroring m
+	    ON m.database_id = d.database_id
+    WHERE d.name NOT IN ('master', 'model', 'msdb', 'tempdb')
+    AND d.is_read_only = 0
+    AND d.user_access_desc = 'MULTI_USER'
+    AND d.is_auto_close_on = 0
+    AND d.recovery_model_desc = 'FULL'
+    AND d.replica_id IS NULL 
+    AND m.mirroring_state IS NULL
+    AND d.source_database_id IS NULL 
+    AND d.state_desc = 'ONLINE' 
+    ORDER BY d.name"
+
+#$DBList = (Invoke-DbaQuery -SqlInstance $SqlInstance -Database master -Query $Query).name
+$DBList = Invoke-DbaQuery -SqlInstance $SqlInstance -Database master -Query $Query
+
 foreach($Database in $DBList)
 {
-    $LastBackup = (Get-DbaLastBackup -SqlInstance $SqlInstance -Database $Database)
-
-    if ($LastBackup.LastFullBackup -eq $null -or [string]::IsNullOrEmpty($LastBackup.LastFullBackup))
+    if ($Database.last_log_backup_lsn -eq $null -or [string]::IsNullOrEmpty($Database.last_log_backup_lsn))
     {
-        # Full database backup to default backup directory.
-        Backup-DbaDatabase -SqlInstance $SqlInstance -Database $Database -CompressBackup -Type Full
+        <#
+            If last_log_backup_lsn IS NULL, the log chain is broken.
+            Take a FULL database backup to default backup directory.
+        #>
+        Backup-DbaDatabase -SqlInstance $SqlInstance -Database $Database.name -CompressBackup -Type Full
     }
 
-    if ($LastBackup.LastLogBackup -eq $null -or [string]::IsNullOrEmpty($LastBackup.LastLogBackup))
-    {
-        # Full database backup to default backup directory.
-        Backup-DbaDatabase -SqlInstance $SqlInstance -Database $Database -CompressBackup -Type Log
-    }
+    <# TODO?: 
+        Compression can be used for automatic seeding, but it is disabled by default. 
+        Turning on compression reduces network bandwidth and possibly speeds up the process, 
+        but the tradeoff is additional processor overhead. To use compression during automatic 
+        seeding, enable trace flag 9567.
 
-    Add-DbaAgDatabase -SqlInstance $SqlInstance -AvailabilityGroup $AvailabilityGroup -Database $Database -SqlCredential $Credential -SeedingMode Automatic
+        Reference: https://docs.microsoft.com/en-us/sql/database-engine/availability-groups/windows/automatic-seeding-secondary-replicas?view=sql-server-2017#considerations
+    #>
+    Add-DbaAgDatabase -SqlInstance $SqlInstance -AvailabilityGroup $AvailabilityGroup -Database $Database -SeedingMode Automatic
 }
